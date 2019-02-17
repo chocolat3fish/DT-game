a7794f692ac685e164d531a97e78be371c37d305
Aidan <aidan@Aidans-MacBook-Air.local>
2019-02-18 11:05:08 +1300

    Camera Movement
    - Follows player
    - has offset (very nice)
    - can adjust speed of camera follow, and default offset.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+
+    public float smoothSpeed;
+    public Vector3 offset;
+
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        Vector3 smoothPositionFinal = new Vector3(smoothPosition.x, smoothPosition.y, -10f);
+
+        transform.position = smoothPositionFinal;
+    }
+}
