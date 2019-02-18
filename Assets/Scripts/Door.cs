383361145361805aaa99c649549f53b05a3c1398
Joshua Browne <joshuabrownenz@gmail.com>
2019-02-18 15:06:21 +1300

    Added pixel art door
    -started Door script but doesn't work

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private void OnCollisionStay(Collision other)
+    {
+
+        Debug.Log(other);
+    }
+}
