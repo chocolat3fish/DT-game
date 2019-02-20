f3ecd9c79faec037e2ed6a3ec3b66e9cfe38d49b
Brownie7 <47587573+Brownie7@users.noreply.github.com>
2019-02-20 20:31:32 +1300

    Started on NPC Dialouge
    Working on NPC mointer script there is a new method called create dialog box not finished yet
    
    created new scene called NPC dialouge there is a NPC sprite when you walk close to it a panel pops up satying click "x" to talk. when you click x a Big Panel pops up and gives you the text. not fully working yet

---
+++ Assets/Scripts/NPCMonitor.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPCMonitor : MonoBehaviour
+{   
+    //different panels in game panels are 
+    public GameObject OverlayPanel;
+    public GameObject toTalkPanel;
+    public GameObject Player;
+
+    public string NPCName;
+
+    private bool canTalk;
+    private bool dialougeBoxOpen;
+
+    public Text mainOverlayText;
+    public Button overlayContinueButton;
+    public Text overlayNameText;
+
+    private void Start()
+    {
+        OverlayPanel.SetActive(false);
+        toTalkPanel.SetActive(false);
+        dialougeBoxOpen = false;
+
+        mainOverlayText = OverlayPanel.transform.Find("Text").GetComponent<Text>();
+        overlayContinueButton = OverlayPanel.transform.Find("Button").GetComponent<Button>();
+        overlayNameText = OverlayPanel.transform.Find("Name").Find("Text").GetComponent<Text>();
+    }
+
+    private void FixedUpdate()
+    {
+        float distance = Vector2.Distance(Player.transform.position, transform.position);
+        if (distance < 3f)
+        {
+            if (!toTalkPanel.activeSelf)
+            {
+                toTalkPanel.SetActive(true);
+            }
+            canTalk = true;
+        }
+        else
+        {
+            if(toTalkPanel.activeSelf)
+            {
+                toTalkPanel.SetActive(false);
+            }
+            canTalk = false;
+        }
+        if (canTalk)
+        {
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                if (!dialougeBoxOpen)
+                {
+                    CreateDialougBox(NPCName);
+                    Debug.Log("dialouge box open");
+                }
+                 
+            }
+
+        }
+    }
+
+    private void CreateDialougBox(string NPCName)
+    {
+        OverlayPanel.SetActive(true);
+        overlayNameText.text = NPCName;
+    }
+
+
+    
+}
