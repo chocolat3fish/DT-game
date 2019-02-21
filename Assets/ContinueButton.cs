35caea125d9690e8c838c237f54db5b6e718ca16
Brownie7 <47587573+Brownie7@users.noreply.github.com>
2019-02-21 20:22:36 +1300

    Added new dialouge class
    This will eventully hold all the infomation for a conversation as well as quests i think. also nice animation with the text as it comes on screen

--- Assets/ContinueButton.cs
+++ Assets/ContinueButton.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ContinueButton : MonoBehaviour
-{
+{   
+    //manager for continue button on overlayCanvas
     public NPCMonitor nPCMonitor;
     private bool continueDialouge = false;
 
